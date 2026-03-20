da7afddd60a45a738efc6c2d6b8840d49b3bc11b
kwon15477 <kwon15477@gmail.com>
2026-03-20 17:42:45 +0900

    Product Entity

--- src/main/java/com/test08/domain/product/entity/Product.java
+++ src/main/java/com/test08/domain/product/entity/Product.java
@@ -1,4 +1,28 @@
 package com.test08.domain.product.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int productId;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private int price;
+
+    private String imageUrl;
+
+    @Column(length = 50)
+    private String category;
 }
