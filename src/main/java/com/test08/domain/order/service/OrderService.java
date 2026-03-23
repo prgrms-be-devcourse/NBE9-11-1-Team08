56e3c261aeee16972a2bd09474698be1b62474e9
Davin <davin11992@gmail.com>
2026-03-23 11:33:36 +0900

    feat: 주문 저장 Service 로직 구현

--- src/main/java/com/test08/domain/order/service/OrderService.java
+++ src/main/java/com/test08/domain/order/service/OrderService.java
@@ -1,4 +1,58 @@
 package com.test08.domain.order.service;
 
+import com.test08.domain.order.dto.OrderForm;
+import com.test08.domain.order.entity.Order;
+import com.test08.domain.order.repository.OrderRepository;
+import com.test08.domain.orderitem.entity.OrderItem;
+import com.test08.domain.orderitem.repository.OrderItemRepository;
+import com.test08.domain.product.entity.Product;
+import com.test08.domain.product.repository.ProductRepository;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
 public class OrderService {
+
+    private final OrderRepository orderRepository;
+    private final OrderItemRepository orderItemRepository;
+    private final ProductRepository productRepository;
+
+    public Order saveOrder(OrderForm form) {
+        Order order = orderRepository
+                .findByEmailAndAddressAndShippedFalse(form.email(), form.address())
+                .orElseGet(() -> orderRepository.save (
+                        Order.create(form.email(), form.address(), form.postCode())
+                ));
+
+        List<OrderItem> existingItems = orderItemRepository.findAllByOrder(order);
+
+        for (var entry : form.items().entrySet()) {
+            Long productId = entry.getKey();
+            int quantity = entry.getValue();
+
+            Product product = productRepository.findById(productId)
+                    .orElseThrow(()-> new IllegalArgumentException("상품 없음 : " +productId));
+
+            existingItems.stream()
+                    .filter(item -> item.getProduct().getProductId().equals((productId)))
+                    .findFirst()
+                    .ifPresentOrElse(
+                            item -> item.addQuantity(quantity),
+                            () -> orderItemRepository.save(
+                                    OrderItem.create(order, product, quantity))
+                    );
+        }
+
+        List<OrderItem> allItems = orderItemRepository.findAllByOrder(order);
+        int total = allItems.stream()
+                .mapToInt(i -> i.getPrice() * i.getQuantity())
+                .sum();
+        order.updateTotalPrice(total);
+
+        return order;
+    }
 }
