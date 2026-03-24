fcc57a28b5f1f258a48a6ae42c48a0e984be4c02
kwon15477 <kwon15477@gmail.com>
2026-03-24 15:15:29 +0900

    feat: 확장자 없는 경로 매핑을 위한 HomeController 구현

--- src/main/java/com/test08/web/HomeController.java
+++ src/main/java/com/test08/web/HomeController.java
@@ -1,4 +1,23 @@
 package com.test08.web;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "forward:/index.html";
+    }
+
+    @GetMapping("/product")
+    public String product() {
+        return "forward:/product.html";
+    }
+
+    @GetMapping("/orders")
+    public String orders() {
+        return "forward:/orders.html";
+    }
 }
