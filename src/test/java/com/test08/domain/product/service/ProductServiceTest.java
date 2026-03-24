0ddbddb1670aaf9980f89fa62c5c5df5abe32ee6
DonggunSe0 <cloudhenry1@gmail.com>
2026-03-24 11:45:52 +0900

    feat : front 오류 수정 및 고도화

--- src/test/java/com/test08/domain/product/service/ProductServiceTest.java
+++ src/test/java/com/test08/domain/product/service/ProductServiceTest.java
@@ -19,7 +19,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-@ExtendWith(MockitoExtension.class)
+1
 class ProductServiceTest {
 
     @InjectMocks
