470a0341bef5fb2244b5b70b8eca778450dac38c
깃허브아이디 <138849418+jonguk-jeong@users.noreply.github.com>
2026-03-24 16:35:31 +0900

    refactor: 우편번호 validation 메시지 개선

--- src/main/java/com/test08/domain/order/dto/OrderForm.java
+++ src/main/java/com/test08/domain/order/dto/OrderForm.java
@@ -10,7 +10,7 @@ import java.util.Map;
 public record OrderForm(
         @Email @NotBlank String email,
         @NotBlank String address,
-        @Pattern(regexp = "\\d{5}") String postCode,
+        @Pattern(regexp = "\\d{5}", message = "우편번호는 5자리 숫자여야 합니다") String postCode,
         @NotEmpty Map<Integer, Integer> items
 ) {
 }
