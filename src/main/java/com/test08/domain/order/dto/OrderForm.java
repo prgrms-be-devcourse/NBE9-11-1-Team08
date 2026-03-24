d5cb97d168ad854bf6df65e2b2abe2ce540e7c7f
jonguk-jeong <138849418+jonguk-jeong@users.noreply.github.com>
2026-03-24 17:11:46 +0900

    feat: 주문 예외처리 추가 (#14)
    * feat: 장바구니 비어있을 때 예외처리 추가
    
    * refactor: 우편번호 validation 메시지 개선
    
    * feat: IllegalArgumentException 예외처리 추가

--- src/main/java/com/test08/domain/order/dto/OrderForm.java
+++ src/main/java/com/test08/domain/order/dto/OrderForm.java
@@ -2,6 +2,7 @@ package com.test08.domain.order.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 
 import java.util.Map;
@@ -9,7 +10,7 @@ import java.util.Map;
 public record OrderForm(
         @Email @NotBlank String email,
         @NotBlank String address,
-        @Pattern(regexp = "\\d{5}") String postCode,
-        Map<Integer, Integer> items
+        @Pattern(regexp = "\\d{5}", message = "우편번호는 5자리 숫자여야 합니다") String postCode,
+        @NotEmpty Map<Integer, Integer> items
 ) {
 }
